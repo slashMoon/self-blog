52f19c33b44f27644f762e46c600eff745cb3c54
slashMoon <sean8922@sina.com>
2024-06-28 20:51:10 +0800

    Revert "Master"

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 
-
 export default async function Page() {
-  let a = 3;
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
   return <Main posts={posts} />
